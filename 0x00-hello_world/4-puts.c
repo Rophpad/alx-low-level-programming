528b0717853d97d5098f6195992ef02efab80392
Rophpad <rophenp@gmail.com>
2023-03-16 06:46:04 -0700

    Commit after Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* main will print a string using the function puts()
+ * and return SUCESS
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
