c61e178b6b5c49a05b5829768c517bd2e865e2ab
Rophpad <rophenp@gmail.com>
2023-03-16 07:04:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/**
- * main -  Print a string using the function puts()
- * return: 0 (SUCCESS)
- */
+/* main - Print a string using the function puts()*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/* Return: 0 (EXIT_SUCCESS)*/
 	return (0);
 }
