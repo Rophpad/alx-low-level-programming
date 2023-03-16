424f862d79035500a3abdb8730887222ba717b3d
Rophpad <rophenp@gmail.com>
2023-03-16 07:21:08 -0700

    Commit after Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text using the function printf()
+ * Return: 0 (EXIT_SUCCESS)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
