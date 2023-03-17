fe11d0927b94efe0a1dac4d74d311460d541d4dd
Rophpad <rophenp@gmail.com>
2023-03-17 04:47:03 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,18 +14,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	switch(n > 0)
+	switch (n > 0)
 	{
-		case(1):
+		case (1):
 			printf("%d is positive\n", n);
 			break;
-		case(0):
-			switch(n < 0)
+		case (0):
+			switch (n < 0)
 			{
-				case(1):
+				case (1):
 					printf("%d is negative\n", n);
 					break;
-				case(0):
+				case (0):
 					printf("%d is zero\n", n);
 					break;
 			}
