83115f4c213bea85ddcb4e436f7a5c47e6ae0f6a
Rophpad <rophenp@gmail.com>
2023-03-21 03:56:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,7 +12,14 @@ int main(void)
 	char text[] = "_putchar";
 	int size = sizeof(text);
 
-	write(1, text, size);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
