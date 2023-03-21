c53e4866c0294273e9c8dfbbb78681699a08e0de
Rophpad <rophenp@gmail.com>
2023-03-21 06:27:31 -0700

    Commit after Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - Find the absolute value of an integer
+ * @number: is the integer
+ *
+ * Return: absvalue (a variable containing the absolute value of
+ * number)
+ */
+int _abs(int number)
+{
+	int absvalue;
+
+	if (number >= 0)
+		absvalue = number;
+	else
+		absvalue = -1*number
+	return (absvalue);
+}
