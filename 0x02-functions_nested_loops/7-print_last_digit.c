1baed1f057f77bb5b0f7ab57e585f5fe7e81454a
Rophpad <rophenp@gmail.com>
2023-03-21 06:53:06 -0700

    Commit after Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+/**
+ * print_last_digit - print the last digit of a number
+ * @number: is the number
+ *
+ * Return: number%10
+ */
+int print_last_digit(int number)
+{
+	return (number % 10);
+}
