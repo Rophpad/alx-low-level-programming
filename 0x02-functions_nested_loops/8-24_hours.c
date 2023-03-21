786ebf9d336b1048161ef0c841e8e34208a2c851
Rophpad <rophenp@gmail.com>
2023-03-21 07:52:17 -0700

    Commit after Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	char h, min;
+
+	for (h = '0'; h < '24'; h++)
+	{
+		if (h < 10)
+		{
+			_putchar('0');
+			_putchar(h);
+		}
+		else
+		{
+			_putchar(h);
+		}
+		_putchar(':');
+		for (min = '0'; min < 60; min++)
+		{
+			if (min < 10)
+			{
+				_putchar('0');
+				_putchar(min);
+			}
+			else
+			{
+				_putchar(min);
+			}
+			_putchar('\n');
+		}
+	}
+}
