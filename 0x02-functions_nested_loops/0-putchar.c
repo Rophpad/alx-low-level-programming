22c3ede9519371412fd3e70e084a84bf7dd6b0cc
Rophpad <rophenp@gmail.com>
2023-03-21 02:59:52 -0700

    Commit after Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints the text _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar\n";
+	int size = sizeof(text);
+
+	write(1, text, size);
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 #This README file contains the descriptions of all the programs of this project
 
+0-putchar.c is a program that prints _putchar, followed by a new line
+
 
