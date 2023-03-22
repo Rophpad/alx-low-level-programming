1f2ebc5dc8d31c803026d3f56478e81aaa837930
Rophpad <rophenp@gmail.com>
2023-03-22 04:43:25 -0700

    Commit after Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - adds two integers and returns the result
+ * @a: is the first integer
+ * @b: is the second
+ *
+ * Return: c (the result of a+b)
+ *
+ */
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
