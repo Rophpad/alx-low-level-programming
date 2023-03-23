1b2fffb4a72de7d951e0ca48a7ce6c174118c6a7
Rophpad <rophenp@gmail.com>
2023-03-23 12:46:47 -0700

    Conforming 0-isupper.c with betty coding style & improve my code

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,17 @@
 
 /**
  * _isupper - Checks for uppercase character
- * @c: is an integer (ascii value of a char )
+ * @c: is an integer (ascii value of a char)
  *
  * Return: bin (1 or 0)
  */
 int _isupper(int c)
 {
 	int bin;
-	if (c >= 97 && c <= 122)
-		bin = 0;
+
 	if (c >= 65 && c <= 90)
 		bin = 1;
+	else
+		bin = 0;
 	return (bin);
 }
