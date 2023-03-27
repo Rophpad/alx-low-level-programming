dec88020d9625aed1c71e0a36eda8c750816a099
Rophpad <rophenp@gmail.com>
2023-03-27 14:08:38 -0700

    Commit after Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string + new line
+ * @str: a string
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar (*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
