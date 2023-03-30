70697b4b80f2bd9d04d269d45f16cd1c340ae84e
Rophpad <rophenp@gmail.com>
2023-03-30 14:41:41 -0700

    Commit after Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,12 +16,12 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		i++;
-		if (src[i] = '\0')
+		if (src[i] == '\0')
 			break;
 	}
 	if (i > n)
 	{
-		dest[i] = '\0'
+		dest[i] = '\0';
 	}
 
 	return (dest);
