963b87e76d4563ca37899f49610eb96354db5fa7
Rophpad <rophenp@gmail.com>
2023-04-03 15:59:00 -0700

    Commit aftet Task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointers to char
+ * @s: a pointers to a pointer that point to a char
+ * @to: a pointer to the char
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to
+}
