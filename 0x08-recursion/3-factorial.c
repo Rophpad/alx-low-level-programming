b5961760d5adbd3d68609be57fc85095267bf96a
Rophpad <rophenp@gmail.com>
2023-04-05 17:24:12 -0700

    Adding comment in 3-factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * factorial - Displays the factorial of a given number
+ * @n: the number
+ *
+ * Return: 0, -1 or factorial of n dependly of the given number
  *
  */
 int factorial(int n)
