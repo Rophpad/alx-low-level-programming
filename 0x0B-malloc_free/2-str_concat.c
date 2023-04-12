721b147e44d4248c91cb64d3eaedeb5a5200f02e
Rophpad <rophenp@gmail.com>
2023-04-12 15:20:15 -0700

    Delette s1=s2=NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int i, len1, len2, j;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = j = len1 = len2 = 0;
 	while (s1[len1] != '\0')
 	{
