408f3d052b1cbfe2fbc9f0b5d18b7e0a94eb7032
Rophpad <rophenp@gmail.com>
2023-04-17 07:39:30 -0700

    Commit after Task 2

---
+++ 0x0E-structures_typedef/init.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a variable of type struct dog
+ * @d: pointer to struct
+ * @name: the name of dog
+ * @age: dog's age
+ * @owner: Dog's owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	/*d = malloc(sizeof(struct dog));*/
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
