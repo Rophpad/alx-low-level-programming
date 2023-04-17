eac2cb83f388290817caae5fea6e37e5e1876ffc
Rophpad <rophenp@gmail.com>
2023-04-17 08:46:06 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#ifndef ABS_VALUE
+#define ABS_VALUE
+
+#define ABS(x) ((x) < (0) ? ((-1) * (x)) : (x))
+
 #endif
