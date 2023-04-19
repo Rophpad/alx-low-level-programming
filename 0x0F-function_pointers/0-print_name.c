a3882bdd28e34a1b862da9b1a6e2a15eebbd017d
Rophpad <rophenp@gmail.com>
2023-04-19 05:04:57 -0700

    Including the header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,11 @@
+#include "function_pointers.h"
+
 /**
  * print_name - print a name
  * @name: Name to print
  * @f: Pointer to void function, taking a pointer to a char as argument
+ *
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
