49387b256d63662e68a2ce4ac29d333c33985c71
Rophpad <rophenp@gmail.com>
2023-04-27 15:46:50 -0700

    Add the function description and fix code

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,15 @@
 #include "lists.h"
 
-/*
+/**
+ * free_list - Frees a list list_t
+ * @head: a pointer to the list_t list
  *
  */
 void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		temp = head->next;
 		free(head->str);
