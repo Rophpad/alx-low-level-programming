00b257531c3f5aacb634338545a930e7536493ef
Rophpad <rophenp@gmail.com>
2023-05-03 09:19:44 -0700

    Conforme 7-get_nodeint.c to betty coding style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
-		if (head == NULL && i != (index -1))
+		if (head == NULL && i != (index - 1))
 			return (NULL);
 	}
 	indexNode->n = head->n;
