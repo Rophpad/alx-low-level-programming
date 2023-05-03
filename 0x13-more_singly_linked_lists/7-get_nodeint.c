3d662b2319a3e3eade28aa3326c9332bec5d569f
Rophpad <rophenp@gmail.com>
2023-05-03 09:15:15 -0700

    Commit after Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Gets the nth node
+ * @head: node's head
+ * @index: the nth node
+ *
+ * Return: NULL or the nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *indexNode;
+	unsigned int i;
+
+	indexNode = (listint_t *)malloc(sizeof(listint_t));
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL && i != (index -1))
+			return (NULL);
+	}
+	indexNode->n = head->n;
+	indexNode->next = head->next;
+	return (indexNode);
+}
