9007e48ec5f96f4c6f3b1127d3a971bdf9d5b8de
Rophpad <rophenp@gmail.com>
2023-05-03 07:17:32 -0700

    Commit after Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *holder;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -19,5 +19,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = holder;
 	}
-	*head = NULL;
+	head = NULL;
 }
