50224850d2f22bffa0037a47349ec7276db4fbde
Rophpad <rophenp@gmail.com>
2023-05-05 08:13:46 -0700

    Commit after Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - Counts the number of bits needed to be
+ * flipped to get from one number to another
+ * @n: the number
+ * @m: the number to flip n to
+ *
+ * Return: The necessary number of bites
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int to = n ^ m, bits = 0;
+
+	while (to > 0)
+	{
+		bits += (to & 1);
+		to >>= 1;
+	}
+	return (bits);
+}
