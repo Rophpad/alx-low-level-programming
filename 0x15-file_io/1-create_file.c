269b607d79970e764de5313c6d42948de7057014
Rophpad <rophenp@gmail.com>
2023-05-08 14:56:15 -0700

    Task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-		len = sizeof(text_content);
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	if (text_content != NULL)
+	{
+		/*	len = sizeof(text_content);*/
+		while (text_content[len] != '\0')
+			len++;
+		w = write(o, text_content, len);
+	}
 
 	if (o == -1 || w == -1)
 		return (-1);
