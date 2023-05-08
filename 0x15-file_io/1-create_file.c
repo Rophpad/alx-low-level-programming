b40cfc7e64772141787a86633623d18abdd50704
Rophpad <rophenp@gmail.com>
2023-05-08 14:39:59 -0700

    Commit after Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - Create a file
+ * @filename: is the name of the file
+ * @text_content: is the text that will be write in the file
+ *
+ * Return: -1 for fail and 1 for success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len;
+
+	if (filename == NULL)
+		return (-1);
+	len = sizeof(text_content);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
