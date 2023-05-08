b353a774834fb662e4fc321a97b88d639c0f1200
Rophpad <rophenp@gmail.com>
2023-05-08 15:09:27 -0700

    Commit after Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: is the name of the file
+ * @text_content: The string to add at the end of the file
+ *
+ * Return: -1 for all fails 1 for success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int opn, w, cls, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	opn = open(filename, O_WRONLY | O_APPEND);
+	if (text_content != NULL)
+	{
+		len = sizeof(text_content);
+		w = write(opn, text_content, len);
+	}
+	if (opn == -1 || w == -1)
+		return (-1);
+	cls = close(opn);
+	if (cls == -1)
+		return (-1);
+	return (1);
+}
