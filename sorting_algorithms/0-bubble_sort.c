269735af16e24985cd59ae69148c2b0faa9da01f
Rophpad <rophenp@gmail.com>
2023-09-22 06:02:29 -0700

    Bubble sort algo implementation

---
+++ sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending
+ *               order using Bubble sort algorithm
+ * @array: The arraay to be sorted
+ * @size: The number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
