1bb009c471de8c66fcd8f8e3bbb77fda8fa6717c
Rophpad <rophenp@gmail.com>
2023-12-11 16:05:13 +0100

    rm trailling white space

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,6 @@ int binary_search(int *array, size_t size, int value)
 		for (start = 0, end = size - 1; end >= start;)
 		{
 			print_arr(array, start, end);
-			
 			mid = start + (end - start) / 2;
 			if (array[mid] == value)
 				return (mid);
