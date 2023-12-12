56028c1becaf2bceb8971c7b599b598ee7d2fee9
Rophpad <rophenp@gmail.com>
2023-12-12 16:10:14 +0100

    Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,26 +12,27 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, jump, step;
+	size_t temp, bond;
 
+	bond = 0;
 	if (array && size != 0)
 	{
-		step = sqrt(size);
-		for (i = jump = 0; jump < size && array[jump] < value;)
+		while (bond < size && array[bond] < value)
 		{
-			printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-			i = jump;
-			jump += step;
+			temp = bond;
+			bond  = bond + sqrt(size);
+			printf("Value checked array[%ld] = [%d]\n", temp, array[temp]);
 		}
-
-		printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
-
-		jump = jump < size - 1 ? jump : size - 1;
-		for (; i < jump && array[i] < value; i++)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
-		return (array[i] == value ? (int)i : -1);
+		if (array[bond] >= value || bond > size)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", temp, bond);
+			for (temp = temp; temp <= size - 1; temp++)
+			{
+				printf("Value checked array[%ld] = [%d]\n", temp, array[temp]);
+				if (array[temp] == value)
+					return (temp);
+			}
+		}
 	}
 	return (-1);
 }
