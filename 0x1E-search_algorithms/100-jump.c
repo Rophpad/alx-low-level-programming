c5f88cae0cfe429ff37d41b18bc9f67175177db1
Rophpad <rophenp@gmail.com>
2023-12-12 16:16:13 +0100

    Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 			{
 				printf("Value checked array[%ld] = [%d]\n", temp, array[temp]);
 				if (array[temp] == value)
-					return (temp);
+					return ((int)temp);
 			}
 		}
 	}
